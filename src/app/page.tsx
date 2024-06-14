74905cbad955b5460653766b1713b44029c219c8
oskvr37 <oskarxpd@gmail.com>
2024-06-14 22:02:32 +0200

    🚀 created app

---
+++ src/app/page.tsx
@@ -0,0 +1,15 @@
+export default function Page() {
+	return (
+		<main>
+			<section className="space-y-4">
+				<h1 className="text-2xl">Check out your stats and insights!</h1>
+				<p className="max-w-96">
+					Welcome to{" "}
+					<span className="text-cyan-500 font-bold">Rift Insight!</span> You can
+					check out your League of Legends progress and learn more about your
+					playstyle.
+				</p>
+			</section>
+		</main>
+	);
+}
