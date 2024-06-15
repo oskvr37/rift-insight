c99f131e01a7b2f728dbbc9343bbf22255174ba4
oskvr37 <oskarxpd@gmail.com>
2024-06-15 23:52:41 +0200

    add more endpoints

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,6 +1,23 @@
 const RIOT_API_KEY = process.env.RIOT_API_KEY;
 
 type REGIONS = "americas" | "asia" | "europe" | "esports";
+type SERVERS =
+	| "BR1"
+	| "EUN1"
+	| "EUW1"
+	| "JP1"
+	| "KR"
+	| "LA1"
+	| "LA2"
+	| "NA1"
+	| "OC1"
+	| "PH2"
+	| "RU"
+	| "SG2"
+	| "TH2"
+	| "TR1"
+	| "TW2"
+	| "VN2";
 
 export async function fetchApi(url: string) {
 	if (!RIOT_API_KEY) {
@@ -20,11 +37,73 @@ export async function fetchApi(url: string) {
 	return await request.json();
 }
 
-export async function byRiotId(
+export async function accountByRiotId(
 	gameName: string,
 	tagLine: string,
 	region: REGIONS
 ): Promise<{ puuid: string; gameName: string; tagLine: string }> {
-	const url = `https://${region}.api.riotgames.com/riot/account/v1/accounts/by-riot-id/${gameName}/${tagLine}`;
-	return await fetchApi(url);
+	return await fetchApi(
+		`https://${region}.api.riotgames.com/riot/account/v1/accounts/by-riot-id/${gameName}/${tagLine}`
+	);
+}
+
+export async function summonerByPuuid(
+	puuid: string,
+	server: SERVERS
+): Promise<{
+	id: string;
+	accountId: string;
+	puuid: string;
+	profileIconId: number;
+	revisionDate: number;
+	summonerLevel: number;
+}> {
+	return await fetchApi(
+		`https://${server}.api.riotgames.com/lol/summoner/v4/summoners/by-puuid/${puuid}`
+	);
+}
+
+export async function matchesByPuuid(
+	puuid: string,
+	region: REGIONS,
+	query?: {
+		startTime?: number;
+		endTime?: number;
+		queue?: number;
+		type?: string;
+		start?: number;
+		count?: number;
+	}
+): Promise<string[]> {
+	const queryString = Object.entries(query || {})
+		.map(([key, value]) => `${key}=${value}`)
+		.join("&");
+
+	return await fetchApi(
+		`https://${region}.api.riotgames.com/lol/match/v5/matches/by-puuid/${puuid}/ids?${queryString}`
+	);
+}
+
+export async function leagueBySummoner(
+	id: string,
+	server: SERVERS
+): Promise<
+	{
+		leagueId: string;
+		queueType: string;
+		tier: string;
+		rank: string;
+		summonerId: string;
+		leaguePoints: number;
+		wins: number;
+		losses: number;
+		veteran: boolean;
+		inactive: boolean;
+		freshBlood: boolean;
+		hotStreak: boolean;
+	}[]
+> {
+	return await fetchApi(
+		`https://${server}.api.riotgames.com/lol/league/v4/entries/by-summoner/${id}`
+	);
 }
