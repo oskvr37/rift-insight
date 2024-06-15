04881777cccf3d67c461ff4fd61e5ef7313c8822
oskvr37 <oskarxpd@gmail.com>
2024-06-15 15:33:02 +0200

    Add fetch function

---
+++ src/utils/api.ts
@@ -0,0 +1,13 @@
+const RIOT_API_KEY = process.env.RIOT_API_KEY;
+
+async function fetchApi(url: string) {
+	if (!RIOT_API_KEY) {
+		throw new Error("RIOT_API_KEY env variable is not set.");
+	}
+
+	return await fetch(url, {
+		headers: {
+			"X-Riot-Token": RIOT_API_KEY,
+		},
+	});
+}
