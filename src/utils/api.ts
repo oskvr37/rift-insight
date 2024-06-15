9dd31b4102019ee4f4ff75a749306d2727d6304a
oskvr37 <oskarxpd@gmail.com>
2024-06-15 21:16:23 +0200

    feat: Add jest

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,6 +1,6 @@
 const RIOT_API_KEY = process.env.RIOT_API_KEY;
 
-async function fetchApi(url: string) {
+export async function fetchApi(url: string) {
 	if (!RIOT_API_KEY) {
 		throw new Error("RIOT_API_KEY env variable is not set.");
 	}
