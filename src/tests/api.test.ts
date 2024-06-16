7a7ab4ee7ef045f0c24eae21d291d91f0fd050ca
oskvr37 <oskarxpd@gmail.com>
2024-06-16 18:06:07 +0200

    add riot api tests

---
+++ src/tests/api.test.ts
@@ -0,0 +1,53 @@
+import {
+	summonerByPuuid,
+	accountByRiotId,
+	matchesByPuuid,
+	leagueBySummoner,
+	championMasteryByPuuid,
+	REGIONS,
+	SERVERS,
+} from "@/utils/api";
+
+describe("Riot API ⚡", () => {
+	const GAME_NAME = "KC NEXT ADKING";
+	const TAG_LINE = "euw";
+	const REGION: REGIONS = "europe";
+	const SERVER: SERVERS = "EUW1";
+
+	let puuid = "";
+	let summonerId = "";
+
+	test("accountByRiotId", async () => {
+		const data = await accountByRiotId(GAME_NAME, TAG_LINE, REGION);
+		expect(data.puuid).toBeDefined();
+		puuid = data.puuid;
+	});
+
+	test("summonerByPuuid", async () => {
+		const summoner = await summonerByPuuid(puuid, SERVER);
+		expect(summoner).toBeDefined();
+		summonerId = summoner.id;
+
+		console.log({ puuid, summonerId });
+	});
+
+	// TODO Test query parameters for functions
+	// and test the response data
+
+	describe("detailed data", () => {
+		test("matchesByPuuid", async () => {
+			const matches = await matchesByPuuid(puuid, REGION);
+			expect(matches).toBeDefined();
+		});
+
+		test("leagueBySummoner", async () => {
+			const leagues = await leagueBySummoner(summonerId, SERVER);
+			expect(leagues).toBeDefined();
+		});
+
+		test("championMasteryByPuuid", async () => {
+			const mastery = await championMasteryByPuuid(puuid, SERVER);
+			expect(mastery).toBeDefined();
+		});
+	});
+});
