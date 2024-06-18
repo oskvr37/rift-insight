3c0eccc6b92ee6927f3a04b6eb4e67bd670889e1
oskvr37 <oskarxpd@gmail.com>
2024-06-18 16:30:18 +0200

    Add closestRegion helper

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,5 +1,29 @@
+import { SERVERS, REGIONS } from "@/types";
+
 export function queryToString(query: Record<string, any>) {
 	return Object.entries(query)
 		.map(([key, value]) => `${key}=${value}`)
 		.join("&");
+}
+
+export function closestRegion(server: SERVERS) {
+	// find closest region to server
+	let region: REGIONS = "asia";
+	if (
+		server === "BR1" ||
+		server === "LA1" ||
+		server === "LA2" ||
+		server === "NA1"
+	) {
+		region = "americas";
+	} else if (
+		server === "EUN1" ||
+		server === "EUW1" ||
+		server === "RU" ||
+		server === "TR1"
+	) {
+		region = "europe";
+	}
+
+	return region;
 }
