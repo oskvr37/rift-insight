6501856dfa72c99e49a4cf3a119cb4aa8c466bd7
oskvr37 <oskarxpd@gmail.com>
2024-06-18 21:06:04 +0200

    ✨ normalize server names

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,13 +1,16 @@
 "use client";
 
 import { searchUser } from "@/actions/search";
-import { useState } from "react";
-import { SERVERS } from "@/types";
+import { useEffect, useState } from "react";
+import { SERVERS, SERVERS_NORMALIZED } from "@/types";
 
 export default function SearchBar() {
 	const storageServer = getStorageServer();
 
 	const [server, setServer] = useState<SERVERS>(storageServer);
+	const [serverNormalized, setServerNormalized] = useState<SERVERS_NORMALIZED>(
+		SERVERS_NORMALIZED[storageServer]
+	);
 	const [inputValue, setInputValue] = useState("");
 	const [error, setError] = useState<string | null>(null);
 
@@ -26,43 +29,49 @@ export default function SearchBar() {
 		searchUser(summonerName, tagLine, server);
 	}
 
+	useEffect(() => {
+		setServerNormalized(SERVERS_NORMALIZED[server]);
+		setStorageServer(server);
+	}, [server]);
+
 	return (
-		<form onSubmit={handleSubmit} className="flex gap-2 fadein">
-			<div className="flex rounded bg-slate-800 w-full">
-				<select
-					className="bg-slate-700 rounded px-2"
-					value={server}
-					required
-					onChange={(event) => {
-						setServer(event.target.value as SERVERS);
-						setStorageServer(event.target.value as SERVERS);
-					}}
+		<section>
+			{error && <p className="text-red-400 text-sm">{error}</p>}
+			<form onSubmit={handleSubmit} className="flex gap-2 fadein">
+				<div className="flex rounded bg-slate-800 w-full">
+					<select
+						className="bg-slate-700 rounded px-2"
+						value={server}
+						required
+						onChange={(event) => {
+							setServer(event.target.value as SERVERS);
+						}}
+					>
+						{Object.entries(SERVERS_NORMALIZED).map(([key, value]) => (
+							<option key={key} value={key}>
+								{value}
+							</option>
+						))}
+					</select>
+					<input
+						className="px-2 py-1 w-full"
+						required
+						type="text"
+						placeholder={`name#${serverNormalized}`}
+						// pattern for summoner name#tagline
+						pattern="^[a-zA-Z0-9 ]+#[a-zA-Z0-9 ]+$"
+						value={inputValue}
+						onChange={(event) => setInputValue(event.target.value)}
+					/>
+				</div>
+				<button
+					type="submit"
+					className="px-2 py-1 rounded bg-cyan-500 text-zinc-900 font-bold"
 				>
-					{Object.entries(SERVERS).map(([key, value]) => (
-						<option key={key} value={value}>
-							{value}
-						</option>
-					))}
-				</select>
-				<input
-					className="px-2 py-1 w-full"
-					required
-					type="text"
-					placeholder={`name#${server}`}
-					// pattern for summoner name#tagline
-					pattern="^[a-zA-Z0-9 ]+#[a-zA-Z0-9 ]+$"
-					value={inputValue}
-					onChange={(event) => setInputValue(event.target.value)}
-				/>
-			</div>
-			<button
-				type="submit"
-				className="px-2 py-1 rounded bg-cyan-500 text-zinc-900 font-bold"
-			>
-				Search
-			</button>
-			{error && <p className="text-red-400">{error}</p>}
-		</form>
+					Search
+				</button>
+			</form>
+		</section>
 	);
 }
 
--- src/types/index.ts
+++ src/types/index.ts
@@ -20,3 +20,43 @@ export const SERVERS = [
 
 export type REGIONS = typeof REGIONS[number];
 export type SERVERS = typeof SERVERS[number];
+
+// export const SERVERS_NORMALIZED = {
+// 	BR: "BR1",
+// 	EUW: "EUW1",
+// 	EUNE: "EUN1",
+// 	JP: "JP1",
+// 	KR: "KR",
+// 	LAN: "LA1",
+// 	LAS: "LA2",
+// 	NA: "NA1",
+// 	OCE: "OC1",
+// 	TR: "TR1",
+// 	RU: "RU",
+// 	SG: "SG",
+// 	VN: "VN",
+// 	TW: "TW",
+// 	TH: "TH",
+// 	PH: "PH",
+// };
+
+export const SERVERS_NORMALIZED = {
+	BR1: "BR",
+	EUW1: "EUW",
+	EUN1: "EUNE",
+	JP1: "JP",
+	KR: "KR",
+	LA1: "LAN",
+	LA2: "LAS",
+	NA1: "NA",
+	OC1: "OCE",
+	TR1: "TR",
+	RU: "RU",
+	SG2: "SG",
+	VN2: "VN",
+	TW2: "TW",
+	TH2: "TH",
+	PH2: "PH",
+}
+
+export type SERVERS_NORMALIZED = typeof SERVERS_NORMALIZED[keyof typeof SERVERS_NORMALIZED];
