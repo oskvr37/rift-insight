8d84c6b671ac9acbfe1ceb090bdfe248a6ba5499
oskvr37 <oskarxpd@gmail.com>
2024-06-18 18:57:52 +0200

    📦 refactoring types from const

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,18 +1,22 @@
-export type REGIONS = "americas" | "asia" | "europe" | "esports";
-export type SERVERS =
-	| "BR1"
-	| "EUN1"
-	| "EUW1"
-	| "JP1"
-	| "KR"
-	| "LA1"
-	| "LA2"
-	| "NA1"
-	| "OC1"
-	| "PH2"
-	| "RU"
-	| "SG2"
-	| "TH2"
-	| "TR1"
-	| "TW2"
-	| "VN2";
+export const REGIONS = ["americas", "asia", "europe", "esports"] as const;
+export const SERVERS = [
+	"BR1",
+	"EUN1",
+	"EUW1",
+	"JP1",
+	"KR",
+	"LA1",
+	"LA2",
+	"NA1",
+	"OC1",
+	"PH2",
+	"RU",
+	"SG2",
+	"TH2",
+	"TR1",
+	"TW2",
+	"VN2",
+] as const;
+
+export type REGIONS = typeof REGIONS[number];
+export type SERVERS = typeof SERVERS[number];
