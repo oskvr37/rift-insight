06a63d9b2766505de5c140fd70678bcbde346d95
oskvr37 <oskarxpd@gmail.com>
2024-06-20 15:28:24 +0200

    ✨ add summoner page

---
+++ src/app/summoner/[server]/[summoner]/page.tsx
@@ -0,0 +1,51 @@
+import { accountByRiotId, summonerByPuuid } from "@/utils/api";
+import { SERVERS, SERVERS_NORMALIZED, SERVERS_UNNORMALIZED } from "@/types";
+import { closestRegion } from "@/utils/helpers";
+import { notFound, redirect } from "next/navigation";
+
+export default async function Page({
+	params,
+}: {
+	params: { server: SERVERS_NORMALIZED; summoner: string };
+}) {
+	const server = params.server.toUpperCase();
+	const summoner = params.summoner;
+	// validate params
+	if (typeof server !== "string" || typeof summoner !== "string") notFound();
+	// check if server is valid
+	if (!SERVERS_UNNORMALIZED[server as keyof typeof SERVERS_UNNORMALIZED])
+		notFound();
+
+	const split = summoner.split("-");
+
+	if (split.length !== 2) notFound();
+
+	const gameName = decodeURIComponent(split[0]);
+	const tagLine = split[1];
+	const riotServer = SERVERS_UNNORMALIZED[
+		server as keyof typeof SERVERS_UNNORMALIZED
+	] as SERVERS;
+
+	const region = closestRegion(riotServer);
+	const riotAccount = await accountByRiotId(gameName, tagLine, region);
+
+	if (!riotAccount) notFound();
+
+	// check if server is valid
+	const summonerData = await summonerByPuuid(riotAccount.puuid, riotServer);
+
+	if (!summonerData) {
+		redirect(`/summoner/${server}`);
+	}
+
+	return (
+		<main>
+			<section>
+				<h1>
+					{riotAccount.gameName} #{riotAccount.tagLine}
+				</h1>
+				<p>{summonerData.summonerLevel} lvl</p>
+			</section>
+		</main>
+	);
+}
