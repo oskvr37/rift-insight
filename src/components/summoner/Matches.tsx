79058ae31111d91c0cb46cb759c704305b711e0d
oskvr37 <oskarxpd@gmail.com>
2024-06-21 23:13:31 +0200

    💩 add Match component

--- src/components/summoner/Matches.tsx
+++ src/components/summoner/Matches.tsx
@@ -1,5 +1,47 @@
 import { REGIONS } from "@/types";
-import { matchesByPuuid } from "@/utils/api";
+import { GameData } from "@/types/match";
+import { matchesByPuuid, matchById } from "@/utils/api";
+
+function Participant({
+	participant,
+}: {
+	participant: GameData["info"]["participants"][0];
+}) {
+	const bg_color = participant.win ? "bg-green-800/25" : "bg-red-800/25";
+
+	return (
+		<div className={`${bg_color} p-2 rounded`}>
+			<span className="font-bold">{participant.summonerName}</span>
+
+			<p>
+				{participant.championName} {participant.kills} / {participant.deaths} /{" "}
+				{participant.assists}
+			</p>
+			<p>{participant.longestTimeSpentLiving} longest seconds alive</p>
+			<p>{participant.totalDamageDealtToChampions} damage dealt</p>
+			<p>{participant.totalDamageTaken} damage taken</p>
+			<p>{participant.totalHeal} healing</p>
+			<p>{participant.visionScore} vision score</p>
+			<p>{participant.goldEarned} gold earned</p>
+			<p>{participant.champLevel} champion level</p>
+			<p>{participant.totalMinionsKilled} minions killed</p>
+			<p>{participant.neutralMinionsKilled} neutral minions killed</p>
+		</div>
+	);
+}
+
+function Match({ match }: { match: GameData }) {
+	return (
+		<div>
+			<h2>{match.metadata.matchId}</h2>
+			<div className="grid grid-cols-5 gap-2">
+				{match.info.participants.map((p) => (
+					<Participant key={p.summonerName} participant={p} />
+				))}
+			</div>
+		</div>
+	);
+}
 
 export default async function SummonerMatches({
 	puuid,
@@ -9,12 +51,16 @@ export default async function SummonerMatches({
 	region: REGIONS;
 }) {
 	const matches = await matchesByPuuid(puuid, region);
+	// const allMatches = await Promise.all(
+	// 	matches.map((m) => matchById(m, region))
+	// );
+	const allMatches = [await matchById(matches[0], region)];
 
 	return (
 		<section>
 			<h1>Matches</h1>
-			{matches.map((m) => (
-				<div key={m}>{m}</div>
+			{allMatches.map((m) => (
+				<Match key={m.metadata.matchId} match={m} />
 			))}
 		</section>
 	);
