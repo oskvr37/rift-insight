ddc824ec2f9f371c820b5b3034d5e674fb8db67e
oskvr37 <oskarxpd@gmail.com>
2024-06-23 15:15:10 +0200

    ✨ use interal api

--- src/components/summoner/Matches.tsx
+++ src/components/summoner/Matches.tsx
@@ -1,15 +1,10 @@
 import { REGIONS } from "@/types";
-import { GameData } from "@/types/match";
-import { matchesByPuuid, matchById } from "@/utils/api";
-
-// 💡 will need internal api for handling matches fetching.
-// one request for user takes around 2MB of data (only 20 matches)
-// so we need to store only neccessary data.
+import { FormattedMatch } from "@/app/api/summoner/[puuid]/match/[match_id]/route";
 
 function Participant({
 	participant,
 }: {
-	participant: GameData["info"]["participants"][0];
+	participant: FormattedMatch["participants"][0];
 }) {
 	const bg_color = participant.win ? "bg-green-800/25" : "bg-red-800/25";
 
@@ -22,25 +17,18 @@ function Participant({
 				{participant.championName} {participant.kills} / {participant.deaths} /{" "}
 				{participant.assists}
 			</p>
-			<p>{participant.longestTimeSpentLiving} longest seconds alive</p>
-			<p>{participant.totalDamageDealtToChampions} damage dealt</p>
-			<p>{participant.totalDamageTaken} damage taken</p>
-			<p>{participant.totalHeal} healing</p>
-			<p>{participant.visionScore} vision score</p>
-			<p>{participant.goldEarned} gold earned</p>
-			<p>{participant.champLevel} champion level</p>
-			<p>{participant.totalMinionsKilled} minions killed</p>
-			<p>{participant.neutralMinionsKilled} neutral minions killed</p>
+			<p>{participant.champLevel} LVL</p>
+			<p>{participant.score} score</p>
 		</div>
 	);
 }
 
-function Match({ match }: { match: GameData }) {
+function Match({ match }: { match: FormattedMatch }) {
 	return (
 		<div>
-			<h2>{match.metadata.matchId}</h2>
+			<h2>{match.matchId}</h2>
 			<div className="grid grid-cols-5 gap-2">
-				{match.info.participants.map((p) => (
+				{match.participants.map((p) => (
 					<Participant key={p.summonerName} participant={p} />
 				))}
 			</div>
@@ -55,17 +43,18 @@ export default async function SummonerMatches({
 	puuid: string;
 	region: REGIONS;
 }) {
-	const matches = await matchesByPuuid(puuid, region);
-	// const allMatches = await Promise.all(
-	// 	matches.map((m) => matchById(m, region))
-	// );
-	const allMatches = [await matchById(matches[0], region)];
+	// 💡 maybe we can just skip /matches route
+	// and fetch cached matches here directly
+	const page = 1;
+	const matches: FormattedMatch[] = await fetch(
+		`http://localhost:3000/api/summoner/${puuid}/matches/${page}`
+	).then((res) => res.json());
 
 	return (
 		<section>
 			<h1>Matches</h1>
-			{allMatches.map((m) => (
-				<Match key={m.metadata.matchId} match={m} />
+			{matches.map((m) => (
+				<Match key={m.matchId} match={m} />
 			))}
 		</section>
 	);
