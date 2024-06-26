d16a1e74aaa15ed7927674e92b764a57760b14fe
oskvr37 <oskarxpd@gmail.com>
2024-06-26 21:03:12 +0200

    ♻️ add new api support

--- src/components/summoner/Matches.tsx
+++ src/components/summoner/Matches.tsx
@@ -2,6 +2,12 @@ import { REGIONS } from "@/types";
 import { FormattedMatch } from "@/app/api/summoner/[puuid]/match/[match_id]/route";
 import { matchesByPuuid } from "@/utils/api";
 import { cache } from "react";
+import { championIcon } from "@/utils/dragon";
+
+type GatheredMatch = FormattedMatch & {
+	won: boolean;
+	player: FormattedMatch["players"][0];
+};
 
 export const gatherMatches = cache(
 	async (puuid: string, region: REGIONS, page: number) => {
@@ -17,7 +23,7 @@ export const gatherMatches = cache(
 				? "https://rift-insight.vercel.app"
 				: "http://localhost:3000";
 
-		const matches: FormattedMatch[] = [];
+		const matches: GatheredMatch[] = [];
 
 		// ⚡ we would use `Promise.all` here to fetch all matches at once
 		// but we are rate limited by not having a production API key
@@ -27,8 +33,12 @@ export const gatherMatches = cache(
 				cache: "force-cache",
 			})
 				.then((res) => res.json())
-				.then((match) => {
-					matches.push(match);
+				.then((match: FormattedMatch) => {
+					matches.push({
+						...match,
+						player: match.players.find((p) => p.info.puuid === puuid)!,
+						won: match.players.find((p) => p.info.puuid === puuid)!.team.win,
+					});
 				});
 		}
 
@@ -44,47 +54,91 @@ export default async function SummonerMatches({
 	region: REGIONS;
 }) {
 	const matches = await gatherMatches(puuid, region, 1);
-	const data: FormattedMatch["participants"][0][] = [];
-
-	matches.map((m) =>
-		m.participants.filter((p) => p.puuid === puuid).map((p) => data.push(p))
-	);
-
-	const winrate = data.filter((p) => p.win).length / data.length;
 
 	return (
-		<section>
-			<h1>Matches</h1>
+		<section className="space-y-2">
 			<div>
-				<h2>Winrate: {100 * winrate}%</h2>
+				<h1>Matches</h1>
+				{/* <div>Winrate, KDA, etc.</div> */}
 			</div>
 			<div className="space-y-2">
-				{data.map((p) => (
-					<Participant key={p.summonerName} participant={p} />
+				{matches.map((match) => (
+					<Match key={match.match_id} match={match} />
 				))}
 			</div>
 		</section>
 	);
 }
 
-function Participant({
-	participant,
-}: {
-	participant: FormattedMatch["participants"][0];
-}) {
-	const bg_color = participant.win ? "bg-green-800/25" : "bg-red-800/25";
+function Match({ match }: { match: GatheredMatch }) {
+	const duration = `${Math.floor(match.duration / 60)}:${(match.duration % 60)
+		.toString()
+		.padStart(2, "0")}`;
 
 	return (
-		<div className={`${bg_color} p-2 rounded`}>
-			<span className="font-bold">{participant.summonerName}</span>
-			{" #"}
-			{participant.riotIdTagline}
-			<p>
-				{participant.championName} {participant.kills} / {participant.deaths} /{" "}
-				{participant.assists}
-			</p>
-			<p>{participant.champLevel} LVL</p>
-			<p>{participant.score} score</p>
-		</div>
+		<article
+			key={match.match_id}
+			className={`rounded p-2 space-y-2 ${
+				match.won
+					? "dark:bg-cyan-900 bg-cyan-400/25"
+					: "dark:bg-slate-800 bg-slate-100"
+			}`}
+		>
+			<div className="flex gap-2 text-xs dark:text-slate-300">
+				<span>
+					{new Date(match.created_at).toLocaleString("en-US", {
+						month: "2-digit",
+						day: "2-digit",
+						year: "2-digit",
+					})}
+				</span>
+				<span>{duration}</span>
+			</div>
+			<div>
+				<p>
+					{match.player.champion.name} {match.player.champion.level} lvl |{" "}
+					{match.player.champion.role}
+				</p>
+				<p>
+					{match.player.stats.kills} / {match.player.stats.deaths} /{" "}
+					{match.player.stats.assists}
+				</p>
+				<p>
+					{match.player.stats.visionScore} vision |{" "}
+					{match.player.stats.totalMinionsKilled} CS |{" "}
+					{match.player.stats.neutralMinionsKilled} jungle
+				</p>
+				<p>
+					Spells {match.player.summoners.spell1} {match.player.summoners.spell2}
+				</p>
+				<p>
+					Physical {match.player.damage.physical} / Magic{" "}
+					{match.player.damage.magic} / True {match.player.damage.true}
+				</p>
+				<p>
+					Runes {match.player.runes.primary} {match.player.runes.secondary}
+				</p>
+				<p>Items {match.player.items.join(" ")}</p>
+				{/* insights */}
+				<div className="flex gap-2">
+					{match.player.insights.firstBlood && <p>First blood</p>}
+					{match.player.insights.killingSpree > 0 && (
+						<p> {match.player.insights.killingSpree} killing spree</p>
+					)}
+					{match.player.insights.multikill > 0 && (
+						<p>{match.player.insights.multikill} multikill</p>
+					)}
+				</div>
+			</div>
+			<div className="flex gap-2">
+				{match.players.map((player) => (
+					<div key={player.champion.id} className="flex gap-2">
+						<div>
+							<img className="size-8" src={championIcon(player.champion.id)} />
+						</div>
+					</div>
+				))}
+			</div>
+		</article>
 	);
 }
