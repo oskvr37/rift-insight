16ea054100bee8b706171260304d0243e87c096b
oskvr37 <oskarxpd@gmail.com>
2024-06-26 14:43:25 +0200

    ✨ add progress bars to recently played

--- src/components/summoner/Recently.tsx
+++ src/components/summoner/Recently.tsx
@@ -2,45 +2,50 @@ import { REGIONS } from "@/types";
 import { gatherMatches } from "./Matches";
 
 export default async function Recently({
-  puuid,
-  region,
-  page
+	puuid,
+	region,
+	page,
 }: {
-  puuid: string;
-  region: REGIONS;
-  page: number;
-
+	puuid: string;
+	region: REGIONS;
+	page: number;
 }) {
-  const matches = await gatherMatches(puuid, region, page);
+	const matches = await gatherMatches(puuid, region, page);
 
-  const playedWith: Record<string, any> = {};
-  matches.map((m) =>
-    m.participants.filter((p) => p.puuid !== puuid).map((p) => {
-      if (playedWith[p.puuid]) {
-        playedWith[p.puuid].games += 1;
-        playedWith[p.puuid].win += p.win ? 1 : 0;
-      } else {
-        playedWith[p.puuid] = { ...p, games: 1, win: p.win ? 1 : 0 };
-      }
-    })
-  );
-  
-  const data = Object.values(playedWith).filter((p) => p.games > 1);
+	const playedWith: Record<string, any> = {};
+	matches.map((m) =>
+		m.participants.map((p) => {
+			if (p.puuid === puuid) return;
+			if (playedWith[p.puuid]) {
+				playedWith[p.puuid].games += 1;
+				playedWith[p.puuid].win += p.win ? 1 : 0;
+			} else {
+				playedWith[p.puuid] = { ...p, games: 1, win: p.win ? 1 : 0 };
+			}
+		})
+	);
 
-  return (
-    <section>
-      <h2>Recently played with</h2>
-      <div className="space-y-2">
-        {data.map((p) => (
-          <div key={p.summonerName} className="dark:bg-slate-800 bg-slate-300 p-2 rounded">
-            <span className="font-bold">{p.summonerName}</span>
-            {" #"}
-            {p.riotIdTagline}
-            <p>Games: {p.games}</p>
-            <p>Winrate: {100 * (p.win / p.games)}%</p>
-          </div>
-        ))}
-      </div>
-    </section>
-  )
+	const data = Object.values(playedWith).filter((p) => p.games > 1);
+
+	return (
+		<section className="space-y-2">
+			<h2>Recently played with</h2>
+			<div className="space-y-2 dark:bg-slate-800 bg-slate-300 p-2 rounded dark:text-slate-300">
+				{data.map((p) => (
+					<div
+						key={p.summonerName}
+						className="flex gap-2 items-center justify-between"
+					>
+						<span className="mr-auto">
+							{p.summonerName} #{p.riotIdTagline}
+						</span>
+						<progress value={p.win} max={p.games} className="h-2 w-1/3" />
+						<span className="dark:text-slate-400 text-sm">
+							{(100 * (p.win / p.games)).toFixed(0)}%
+						</span>
+					</div>
+				))}
+			</div>
+		</section>
+	);
 }
