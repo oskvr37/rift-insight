ed154f73553122aafb41bd4bbb6b792af55f47ae
oskvr37 <oskarxpd@gmail.com>
2024-06-26 13:52:47 +0200

    💄 add header and footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,16 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import dynamic from "next/dynamic";
+import Link from "next/link";
 
 export const metadata: Metadata = {
 	title: "Rift Insight",
 	description: "Check League of Legends player stats and insights!",
 };
+
+const SearchBar = dynamic(() => import("@/components/SearchBar"), {
+	ssr: false,
+});
 
 export default function RootLayout({
 	children,
@@ -13,7 +19,27 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang="en">
-			<body>{children}</body>
+			<body>
+				<header>
+					<div className="flex items-center justify-between">
+						<Link href="/">Rift Insight</Link>
+						<SearchBar />
+					</div>
+				</header>
+				{children}
+				<footer>
+					<div>
+						<p>&copy; 2024 Rift Insight</p>
+						<p className="text-xs">
+							Rift Insight is not endorsed by Riot Games and does not reflect
+							the views or opinions of Riot Games or anyone officially involved
+							in producing or managing League of Legends. League of Legends and
+							Riot Games are trademarks or registered trademarks of Riot Games,
+							Inc. League of Legends © Riot Games, Inc.
+						</p>
+					</div>
+				</footer>
+			</body>
 		</html>
 	);
 }
