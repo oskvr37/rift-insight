47e9093f59345a62120219fc30693c56540d846f
oskvr37 <oskarxpd@gmail.com>
2024-06-29 20:40:43 +0200

    💄 style update

--- src/components/summoner/Mastery.tsx
+++ src/components/summoner/Mastery.tsx
@@ -11,22 +11,33 @@ export default async function SummonerMastery({
 }) {
 	const mastery = await championMasteryByPuuid(puuid, server);
 
+	// 💡 add some champion info to fill layout
+
 	return (
 		<section className="space-y-2">
 			<div className="flex justify-between items-end">
 				<h2>Champion Mastery</h2>
-				<p className="dark:text-slate-300 text-slate-500 text-sm">See more</p>
+				<p className="dark:text-slate-400 text-slate-500 text-sm dark:font-light">
+					See more
+				</p>
 			</div>
-			<div className="flex gap-2 dark:text-slate-300 justify-between dark:bg-slate-800 bg-slate-100 p-2 rounded shadow">
+			<div className="flex gap-2 justify-between">
 				{mastery.map((m) => (
-					<div key={m.championId} className="flex gap-2 items-center">
+					<div
+						key={m.championId}
+						className="w-full flex gap-2 dark:text-slate-300 dark:bg-slate-800 bg-slate-100 p-1 rounded shadow"
+					>
 						<img src={championIcon(m.championId)} alt="" className="size-12" />
 						<div>
-							<p className="space-x-1 text-cyan-400">
-								<span className="text-xl">{m.championLevel}</span>
-								<span>lvl</span>
+							<p className="dark:text-slate-400">
+								<span className="text-xl dark:text-cyan-400">
+									{m.championLevel}
+								</span>{" "}
+								lvl
 							</p>
-							<p>{m.championPoints.toLocaleString()}</p>
+							<p className="text-sm dark:font-light">
+								{m.championPoints.toLocaleString()}
+							</p>
 						</div>
 					</div>
 				))}
