604fd216d7ec6f2de8a4741f1d3ad8d2a73bb2b0
oskvr37 <oskarxpd@gmail.com>
2024-07-04 00:37:33 +0200

    💩 add bad header modals

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,8 +6,40 @@ import {
 	MagnifyingGlassIcon,
 } from "@heroicons/react/16/solid";
 import icon from "@/app/icon.svg";
+import List from "@/components/favorite/List";
+import SearchHistory from "@/components/SearchHistory";
+import SearchBar from "@/components/SearchBar";
+
+import { useState, useCallback } from "react";
 
 export default function Header() {
+	const [currentModal, setCurrentModal] = useState("");
+
+	const openModal = useCallback((modal: string) => {
+		setCurrentModal((prevModal) => (prevModal !== modal ? modal : ""));
+	}, []);
+
+	function renderModal() {
+		switch (currentModal) {
+			case "favorite":
+				return (
+						<List key="List" />
+				);
+			case "search-history":
+				return (
+					<section
+						className="md:grid gap-2 flex flex-col-reverse md:grid-cols-2"
+						key="SearchHistory"
+					>
+						<SearchHistory />
+						<SearchBar />
+					</section>
+				);
+			default:
+				return null;
+		}
+	}
+
 	return (
 		<header className="z-50">
 			<div className="container flex items-center justify-between">
@@ -18,15 +50,25 @@ export default function Header() {
 					</div>
 				</Link>
 				<section className="flex gap-2">
-					<button className="dark:bg-slate-800 p-2 rounded shadow">
+					<button
+						className="dark:bg-slate-800 p-2 rounded shadow"
+						onClick={() => openModal("search-history")}
+					>
 						<MagnifyingGlassIcon className="size-6 text-cyan-500" />
 					</button>
-					<button className="dark:bg-slate-800 p-2 rounded shadow">
+					<button
+						className="dark:bg-slate-800 p-2 rounded shadow"
+						onClick={() => openModal("favorite")}
+					>
 						<StarIconSolid className="size-6 text-cyan-500" />
 					</button>
 				</section>
 			</div>
-			<aside id="portal" />
+			{currentModal && (
+				<aside className="container border-b-2 border-slate-800">
+					{currentModal && renderModal()}
+				</aside>
+			)}
 		</header>
 	);
 }
