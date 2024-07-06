be2337469d914fe102dbc58514099d283f86f244
oskvr37 <oskarxpd@gmail.com>
2024-07-06 22:26:30 +0200

    🐛 custom colors didnt export in build

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,6 +4,7 @@ const config: Config = {
 	content: [
 		"./src/app/**/*.{js,ts,jsx,tsx,mdx}",
 		"./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/utils/**/*.{js,ts,jsx,tsx,mdx}",
 	],
 	theme: {
 		extend: {
